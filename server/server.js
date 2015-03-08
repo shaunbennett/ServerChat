b0929bf0f3a67680e7864623f79a793a7b5e0c72
Shaun Bennett <shaun@mineplex.com>
2015-03-08 19:25:20 -0400

    fix formatting

--- server/server.js
+++ server/server.js
@@ -1,18 +1,18 @@
 var express = require('express'),
-		app = express(),
-		redis = require("redis"),
-		client = redis.createClient(6379, "localhost", {}),
-		clientpub = redis.createClient(6379, "localhost", {}),
-		channels = require('./model/channel.js');
+  app = express(),
+  redis = require("redis"),
+  client = redis.createClient(6379, "localhost", {}),
+  clientpub = redis.createClient(6379, "localhost", {}),
+  channels = require('./model/channel.js');
 
 var server = server = app.listen(3000, function() {
-	console.log("Web server started!");
+  console.log("Web server started!");
 });
 
 var io = require('socket.io').listen(server);
 
 // Log Redis Errors
-client.on("error", function (err) {
+client.on("error", function(err) {
 	console.log("Error " + err);
 });
 
@@ -20,36 +20,36 @@ client.on("error", function (err) {
 client.psubscribe("serverchat.*");
 
 app.set('view engine', 'ejs');
-app.get('/', function (req, res) {
-	res.render('index', {
-		'ip': req.connection.remoteAddress
-	});
+app.get('/', function(req, res) {
+  res.render('index', {
+    'ip': req.connection.remoteAddress
+  });
 });
 
 // socket.io connection from web
-io.on('connection', function (socket) {
-	// Connection Message
-	clientpub.publish(channels.ClientStatusUpdate,
-		socket.handshake.address + " has connected to web client.")
+io.on('connection', function(socket) {
+  // Connection Message
+  clientpub.publish(channels.ClientStatusUpdate,
+    socket.handshake.address + " has connected to web client.")
 
-	client.on('pmessage', function (pattern, channel, message) {
-		var msgObject = {
-			message: message,
-			channel: channel
-		};
+  client.on('pmessage', function(pattern, channel, message) {
+    var msgObject = {
+      message: message,
+      channel: channel
+    };
 
-		socket.emit('chatUpdate', msgObject);
-	});
+    socket.emit('chatUpdate', msgObject);
+  });
 
-	socket.on('chatMessage', function(msg) {
-		clientpub.publish(channels.ClientMessage,
-			socket.handshake.address + " > " + msg);
-	});
+  socket.on('chatMessage', function(msg) {
+    clientpub.publish(channels.ClientMessage,
+      socket.handshake.address + " > " + msg);
+  });
 
-	socket.on('disconnect', function() {
-		clientpub.publish(channels.ClientStatusUpdate,
-			socket.handshake.address + " has disconnected from web client.")
-	});
+  socket.on('disconnect', function() {
+    clientpub.publish(channels.ClientStatusUpdate,
+      socket.handshake.address + " has disconnected from web client.")
+  });
 });
 
 // use public directory for static files
